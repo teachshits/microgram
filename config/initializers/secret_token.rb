c1f3142a309467aaa2e0d820f1b9bb354e899d7e
Yaroslav <kyzon42@gmail.com>
2012-10-31 21:59:26 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microgramm::Application.config.secret_token = '7cd9d7c26976e6425b368acaa686fc6d7637629c73eb1cfa40f17e58338d0fe655b03a3b38961bc9dd326656375c5fd6c8e076a436a02dc232767b1ebb9c7436'
