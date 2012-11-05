756054650eda8d92198f303e3028e60c721b2620
Yaroslav <yaroslav@MacBook-Pro-Yaroslav.local>
2012-11-06 00:52:48 +0400

    sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   before_save { self.email.downcase }  # сохраняем емэйл в нижнем регистре перед сохранением в БД ()
+  before_save :create_remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -16,5 +17,10 @@ class User < ActiveRecord::Base
   		user
   	end
   end
+
+private
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_base64  # возвращает случайную строку длиной в 16, содержащую след. символы: A–Z, a–z, 0–9, “-”, and “_” (для общих 64 вероятностей). Вероятность двух одинаковых токенов крайне мала!
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Microgramm::Application.routes.draw do
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/contact', to: 'pages#contact'
   match '/about',   to: 'pages#about'
   match '/help',    to: 'pages#help'
