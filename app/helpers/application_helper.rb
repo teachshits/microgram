15d508eb18dfbc2eae44a9fff85c42579b7713e3
Yaroslav <yaroslav@MacBook-Pro-Yaroslav.local>
2012-11-05 11:41:11 +0400

    first css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Return a title on a per-page basis.
+  def title
+    base_title = "Microgram App"
+    if @title.nil?
+      base_title
+    else
+      "#{@title}"
+    end
+  end
+
 end
